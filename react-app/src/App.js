01dcf8ce3a77cbfff9ec784e2ac7456454e6ba79
Tianhao Alexander Huang <huangtianhao0920@126.com>
2023-01-19 18:37:32 +0800

    Configure antd and craco
    Signed-off-by: Tianhao Alexander Huang <huangtianhao0920@126.com>

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,11 +4,14 @@ import './index.scss';
 import Login from './pages/Login';
 import Layout from './pages/Layout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Button } from 'antd';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Button type="primary">Button</Button>
         <Routes>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/' element={<Layout />}></Route>
