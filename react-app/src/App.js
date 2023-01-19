c394b18cab431429db60178d9b58086947f2b561
Tianhao Alexander Huang <huangtianhao0920@126.com>
2023-01-19 18:12:54 +0800

    Configure basic router
    Signed-off-by: Tianhao Alexander Huang <huangtianhao0920@126.com>

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.scss';
+import Login from './pages/Login';
+import Layout from './pages/Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/' element={<Layout />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
